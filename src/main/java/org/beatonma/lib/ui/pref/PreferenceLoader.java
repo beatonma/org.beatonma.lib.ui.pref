e1fb6c37ec902304e2ad44f53dcbcda6cc545aa1
Michael Beaton <beatonma@gmail.com>
2018-05-13 17:41:38 +0100

    Now uses SupportBaseAsyncTaskLoader

--- src/main/java/org/beatonma/lib/ui/pref/PreferenceLoader.java
+++ src/main/java/org/beatonma/lib/ui/pref/PreferenceLoader.java
@@ -1,14 +1,15 @@
 package org.beatonma.lib.ui.pref;
 
 import android.content.Context;
-import androidx.annotation.Nullable;
 
-import org.beatonma.lib.load.AsyncTaskResult;
-import org.beatonma.lib.load.BaseAsyncTaskLoader;
+import org.beatonma.lib.load.AsyncResult;
 import org.beatonma.lib.load.SupportBaseAsyncTaskLoader;
 import org.beatonma.lib.ui.pref.preferences.PreferenceGroup;
 
-public class PreferenceLoader extends BaseAsyncTaskLoader<PreferenceGroup> {
+/**
+ * Asynchronously parse a preferences JSON file and return a PreferenceGroup instance
+ */
+public class PreferenceLoader extends SupportBaseAsyncTaskLoader<PreferenceGroup> {
     private final int mResourceID;
 
     public PreferenceLoader(final Context context, final int resourceID) {
@@ -17,46 +18,22 @@ public class PreferenceLoader extends BaseAsyncTaskLoader<PreferenceGroup> {
     }
 
     @Override
-    public AsyncTaskResult<PreferenceGroup> loadInBackground() {
-        final AsyncTaskResult.Builder<PreferenceGroup> result = AsyncTaskResult.getBuilder();
+    public AsyncResult<PreferenceGroup> loadInBackground() {
+        final AsyncResult.Builder<PreferenceGroup> result = AsyncResult.getBuilder();
         final PreferenceGroup prefs = PreferenceGroup.fromJson(getContext(), mResourceID);
+
+        if (prefs.isEmpty()) {
+            result.failure("Loaded preferences are empty - please check your definitions!");
+            return result;
+        }
+
         prefs.load(getContext());
         result.success(prefs);
         return result;
     }
 
     @Override
-    protected void onReleaseResources(final AsyncTaskResult data) {
+    protected void onReleaseResources(final AsyncResult data) {
 
-    }
-
-    public static class SupportPreferenceLoader extends SupportBaseAsyncTaskLoader<PreferenceGroup> {
-        private final int mResourceID;
-
-        public SupportPreferenceLoader(final Context context, final int resourceID) {
-            super(context);
-            mResourceID = resourceID;
-        }
-
-        @Nullable
-        @Override
-        public AsyncTaskResult<PreferenceGroup> loadInBackground() {
-            final AsyncTaskResult.Builder<PreferenceGroup> result = AsyncTaskResult.getBuilder();
-            final PreferenceGroup prefs = PreferenceGroup.fromJson(getContext(), mResourceID);
-
-            if (prefs.isEmpty()) {
-                result.failure("Loaded preferences are empty - please check your definitions!");
-                return result;
-            }
-
-            prefs.load(getContext());
-            result.success(prefs);
-            return result;
-        }
-
-        @Override
-        protected void onReleaseResources(final AsyncTaskResult data) {
-
-        }
     }
 }
