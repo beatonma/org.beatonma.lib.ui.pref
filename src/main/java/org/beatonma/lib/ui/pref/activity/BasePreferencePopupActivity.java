b57aa3058ca9b15a4566a3f2cedc06801b59154a
Michael Beaton <beatonma@gmail.com>
2018-05-15 13:59:06 +0100

    Updated PopupActivity path

--- src/main/java/org/beatonma/lib/ui/pref/activity/BasePreferencePopupActivity.java
+++ src/main/java/org/beatonma/lib/ui/pref/activity/BasePreferencePopupActivity.java
@@ -1,7 +1,5 @@
 package org.beatonma.lib.ui.pref.activity;
 
-import org.beatonma.lib.ui.activity.PopupActivity;
-
-public abstract class BasePreferencePopupActivity<T> extends PopupActivity {
-    public abstract void save(final T obj);
-}
+//public abstract class BasePreferencePopupActivity<T> extends PopupActivity {
+//    public abstract void save(final T obj);
+//}
