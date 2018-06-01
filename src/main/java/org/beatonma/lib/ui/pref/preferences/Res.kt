898ce5db052a6dad157a7c14a8f045a51ef2382b
Michael Beaton <beatonma@gmail.com>
2018-06-01 18:20:23 +0100

    Converted to Kotlin

---
+++ src/main/java/org/beatonma/lib/ui/pref/preferences/Res.kt
@@ -0,0 +1,61 @@
+package org.beatonma.lib.ui.pref.preferences
+
+import android.content.Context
+import java.util.regex.Pattern
+
+/**
+ * Utility methods for reading values from [Resources] using plaintext resource IDs
+ * e.g. "@string/name"
+ */
+
+/**
+ * If the text represents a @resource, return the value of that resource
+ * else return the given text
+ */
+internal fun getString(context: Context,
+                       text: String): String {
+    val resId = getResourceId(context, text)
+    return if (resId == 0) text else context.resources.getString(resId)
+}
+
+internal fun getInt(context: Context,
+                    text: String): Int {
+    val resId = getResourceId(context, text)
+
+    return if (resId == 0) {
+        try {
+            Integer.valueOf(text)
+        } catch (e: Exception) {
+            0
+        }
+    } else {
+        context.resources.getInteger(resId)
+    }
+}
+
+internal fun getIntArray(context: Context,
+                         text: String): IntArray? {
+    val resId = getResourceId(context, text)
+    return if (resId == 0) null else context.resources.getIntArray(resId)
+}
+
+internal fun getStringArray(context: Context,
+                            text: String): Array<String>? {
+    val resId = getResourceId(context, text)
+    return if (resId == 0) null else context.resources.getStringArray(resId)
+}
+
+internal fun getResourceId(context: Context, key: String): Int {
+    val pattern = Pattern.compile("@(\\w+)/(.*)")
+    val m = pattern.matcher(key)
+    if (m.matches()) {
+        val resType = m.group(1)
+        val name = m.group(2)
+        return context.resources.getIdentifier(
+                name,
+                resType,
+                context.packageName)
+    }
+    return 0
+}
+//}
