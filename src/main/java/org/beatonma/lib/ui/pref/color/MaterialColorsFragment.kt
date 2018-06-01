3fb4775dbe4616ad9224339b662e151c295adf09
Michael Beaton <beatonma@gmail.com>
2018-06-01 14:04:29 +0100

    Added transition animations and state restoration

--- src/main/java/org/beatonma/lib/ui/pref/color/MaterialColorsFragment.kt
+++ src/main/java/org/beatonma/lib/ui/pref/color/MaterialColorsFragment.kt
@@ -2,14 +2,20 @@ package org.beatonma.lib.ui.pref.color
 
 import android.app.Application
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
 import androidx.core.view.ViewCompat
 import androidx.core.view.doOnPreDraw
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.transition.Fade
+import androidx.transition.Slide
+import androidx.transition.TransitionSet
+import androidx.transition.Visibility.MODE_IN
 import org.beatonma.lib.prefs.R
 import org.beatonma.lib.prefs.databinding.FragmentColorMaterialBinding
 import org.beatonma.lib.ui.activity.BaseFragment
@@ -17,16 +23,16 @@ import org.beatonma.lib.ui.pref.clone
 import org.beatonma.lib.ui.recyclerview.BaseRecyclerViewAdapter
 import org.beatonma.lib.ui.recyclerview.BaseViewHolder
 
-
 class MaterialColorsFragment : BaseFragment() {
-//        LoaderManager.LoaderCallbacks<AsyncResult<MutableList<Swatch>>> {
 
     companion object {
         const val TAG = "MaterialColorsFrag"
-        private const val LOADER_COLORS = 2389
         private const val VIEW_LEVEL_SWATCHES = 0
         private const val VIEW_LEVEL_COLORS = 1
     }
+
+    override val layoutId: Int = R.layout.fragment_color_material
+
     lateinit var prefViewModel: ColorPreferenceViewModel
     lateinit var swatchViewModel: MaterialColorsViewModel
 
@@ -34,40 +40,38 @@ class MaterialColorsFragment : BaseFragment() {
 
     private val colorsAdapter: PatchAdapter = createAdapter()
 
-//    private val swatches: MutableList<Swatch> = mutableListOf()
     private val visibleColors: MutableList<ColorItem> = mutableListOf()
     private val tempColors: MutableList<ColorItem> = mutableListOf()
 
     private var level = VIEW_LEVEL_SWATCHES
     private var openSwatch = -1
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        with (outState) {
-            putInt("level", level)
-            putInt("openSwatch", openSwatch)
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         prefViewModel = ViewModelProviders.of(activity!!).get(ColorPreferenceViewModel::class.java)
         swatchViewModel = ViewModelProviders.of(this).get(MaterialColorsViewModel::class.java)
 
-//        swatchViewModel.swatches.observe(this, Observer { colorsAdapter.showSwatches() })
-
-        if (savedInstanceState != null) {
-            level = savedInstanceState.getInt("level", VIEW_LEVEL_SWATCHES)
-            openSwatch = savedInstanceState.getInt("openSwatch", -1)
+        val fade = Fade().apply {
+            mode = MODE_IN
+        }
+        val slide = Slide().apply {
+            slideEdge = Gravity.BOTTOM
+            mode = MODE_IN
         }
 
-//        val transition = TransitionInflater.from(context)
-//                .inflateTransition(R.transition.custom_color_unshared)
-//        enterTransition = transition
-//        exitTransition = transition
+        val transition = TransitionSet().apply {
+            duration = 300
+            interpolator = OvershootInterpolator(0.6F)
+            addTransition(fade)
+            addTransition(slide)
+        }
+
+        enterTransition = transition
+        returnTransition = transition
+        reenterTransition = transition
     }
 
-    override fun init(binding: ViewDataBinding?) {
+    override fun init(binding: ViewDataBinding) {
         this.binding = binding as FragmentColorMaterialBinding
 
         binding.recyclerview.apply {
@@ -75,8 +79,6 @@ class MaterialColorsFragment : BaseFragment() {
             layoutManager = GridLayoutManager(context, 4)
             itemAnimator = ColorItemAnimator(gridWidth = 4)
         }
-
-//        swatchViewModel.swatches.observe(this, Observer { colorsAdapter.showSwatches() })
 
         getColorActivity()?.apply {
             onCustomActionClick(
@@ -87,12 +89,6 @@ class MaterialColorsFragment : BaseFragment() {
             )
             onPositiveClick()
         }
-
-//        binding.customButton.setOnClickListener {
-//            getColorActivity()?.customiseColor(prefViewModel!!.colorPreference.value?.color ?: 0x000000)
-//        }
-
-//        LoaderManager.getInstance(this).initLoader(LOADER_COLORS, null, this)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -102,21 +98,30 @@ class MaterialColorsFragment : BaseFragment() {
         (view.parent as? ViewGroup)?.doOnPreDraw {
             startPostponedEnterTransition()
         }
-        colorsAdapter.showSwatches()
+
+        savedInstanceState?.run {
+            level = getInt("level", VIEW_LEVEL_SWATCHES)
+            openSwatch = getInt("openSwatch", -1)
+        }
+
+        if (level == VIEW_LEVEL_COLORS && openSwatch >= 0) {
+            colorsAdapter.showSwatch(openSwatch)
+        } else {
+            colorsAdapter.showSwatches()
+        }
     }
 
-    override fun getLayoutId(): Int {
-        return R.layout.fragment_color_material
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        with(outState) {
+            putInt("level", level)
+            putInt("openSwatch", openSwatch)
+        }
     }
 
     private fun getColorItemAnimator(): ColorItemAnimator? {
         return binding?.recyclerview?.itemAnimator as ColorItemAnimator
     }
-
-//    override fun onCreateLoader(id: Int, args: Bundle?):
-//            Loader<AsyncResult<MutableList<Swatch>>> {
-//        return MaterialColorsLoader(context!!)
-//    }
 
     fun onBackPressed(): Boolean {
         if (!isDetached && level == VIEW_LEVEL_COLORS) {
@@ -127,7 +132,7 @@ class MaterialColorsFragment : BaseFragment() {
     }
 
     fun createAdapter(): PatchAdapter {
-        return PatchAdapter(object: ColorSelectedCallback {
+        return PatchAdapter(object : ColorSelectedCallback {
             override fun onColorSelected(color: Int, swatch: Int, swatchPosition: Int) {
                 val pref = prefViewModel.colorPreference.value
                 pref?.update(color, swatch, swatchPosition)
@@ -136,33 +141,6 @@ class MaterialColorsFragment : BaseFragment() {
             }
         })
     }
-
-//    override fun onLoadFinished(loader: Loader<AsyncResult<MutableList<Swatch>>>,
-//                                result: AsyncResult<MutableList<Swatch>>) {
-//        when (loader.id) {
-//            LOADER_COLORS -> {
-////                visibleColors.clear()
-////                colorsAdapter.notifyDataSetChanged()
-//                swatches.clone(result.data)
-//                if (!isDetached) {
-//                    colorsAdapter.showSwatches()
-//                }
-//                else Log.d(TAG, "result received while fragment detached")
-//            }
-//        }
-//    }
-
-//    override fun onAttach(context: Context?) {
-//        super.onAttach(context)
-//        Log.d(TAG, "onAttach")
-//        if (swatches.isNotEmpty()) {
-//            Log.d(TAG, "showing swatches")
-//            visibleColors.clear()
-//            colorsAdapter.showSwatches()
-//        }
-//    }
-
-//    override fun onLoaderReset(loader: Loader<AsyncResult<MutableList<Swatch>>>) {}
 
     private fun getColorActivity(): SwatchColorPreferenceActivity? {
         return activity as SwatchColorPreferenceActivity
@@ -173,14 +151,8 @@ class MaterialColorsFragment : BaseFragment() {
             return visibleColors.size
         }
 
-//        override fun getItems(): MutableList<*> {
-//            return visibleColors
-//        }
-
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
             return PatchViewHolder(inflate(parent, R.layout.vh_pref_color_patch))
-//                else -> super.onCreateViewHolder(parent, viewType)
-//            }
         }
 
         inner class PatchViewHolder(view: View) : BasePatchViewHolder(view) {
@@ -194,8 +166,7 @@ class MaterialColorsFragment : BaseFragment() {
                         "${getString(R.string.transition_color_preview)}_$position")
 
                 patch.setOnLongClickListener {
-                    (activity as SwatchColorPreferenceActivity)
-                            .customiseColor(patch.color, sharedView = patch)
+                    getColorActivity()?.customiseColor(patch.color, sharedView = patch)
                     true
                 }
 
@@ -207,10 +178,10 @@ class MaterialColorsFragment : BaseFragment() {
                     }
                     VIEW_LEVEL_COLORS -> {
                         patch.chosen = false
-                            patch.choose(
-                                    openSwatch == colorPref?.swatch
-                                            && position == colorPref.swatchPosition,
-                                    true)
+                        patch.choose(
+                                openSwatch == colorPref?.swatch
+                                        && position == colorPref.swatchPosition,
+                                true)
 
                         patch.setOnClickListener { selectColor(patch, position) }
                     }
@@ -242,23 +213,17 @@ class MaterialColorsFragment : BaseFragment() {
             swatchViewModel.swatches.let {
                 tempColors.clone(it[position].colors)
             }
-//            tempColors.clone(swatches[position].colors)
             applyDiff(visibleColors, tempColors)
             visibleColors.clone(tempColors)
         }
-
 
         /**
          * We don't want views to move around - they should only be added, removed or
          * change in-place - so the normal DiffUtils diff method is not ideal.
          */
-        fun applyDiff(old: MutableList<ColorItem>?, new: MutableList<ColorItem>?) {
-            if (old == null && new == null) {
-                return
-            }
-
-            val oldSize = old?.size ?: 0
-            val newSize = new?.size ?: 0
+        fun applyDiff(old: MutableList<ColorItem>, new: MutableList<ColorItem>) {
+            val oldSize = old.size
+            val newSize = new.size
 
             if (oldSize > newSize) notifyItemRangeRemoved(newSize, oldSize - newSize)
             else if (oldSize < newSize) notifyItemRangeInserted(oldSize, newSize - oldSize)
@@ -270,7 +235,7 @@ class MaterialColorsFragment : BaseFragment() {
     }
 }
 
-class MaterialColorsViewModel(context: Application): AndroidViewModel(context) {
+class MaterialColorsViewModel(context: Application) : AndroidViewModel(context) {
     val swatches: List<Swatch>
 
     init {
@@ -305,47 +270,5 @@ class MaterialColorsViewModel(context: Application): AndroidViewModel(context) {
         }
     }
 }
-
-
-//class MaterialColorsLoader(context: Context) : SupportBaseAsyncTaskLoader<MutableList<Swatch>>(context) {
-//    override fun onReleaseResources(data: AsyncResult<MutableList<Swatch>>) {}
-//
-//    override fun loadInBackground(): AsyncResult<MutableList<Swatch>>? {
-//        val builder: AsyncResult.Builder<MutableList<Swatch>> = AsyncResult.getBuilder()
-//
-//        val defs = intArrayOf(
-//                R.array.material_red,
-//                R.array.material_pink,
-//                R.array.material_purple,
-//                R.array.material_deep_purple,
-//                R.array.material_indigo,
-//                R.array.material_blue,
-//                R.array.material_light_blue,
-//                R.array.material_cyan,
-//                R.array.material_teal,
-//                R.array.material_green,
-//                R.array.material_light_green,
-//                R.array.material_lime,
-//                R.array.material_yellow,
-//                R.array.material_amber,
-//                R.array.material_orange,
-//                R.array.material_deep_orange,
-//                R.array.material_blue_grey,
-//                R.array.material_brown,
-//                R.array.material_grey
-//        )
-//        val swatches: MutableList<Swatch> = MutableList(defs.size) {
-//            val swatchDef: IntArray = context.resources.getIntArray(defs[it])
-//            val colors: MutableList<ColorItem> = MutableList(swatchDef.size) { id ->
-//                ColorItem(swatchDef[id])
-//            }
-//            val s = Swatch(colors, colors[5])
-//            s
-//        }
-//        builder.success(swatches)
-//
-//        return builder
-//    }
-//}
 
 data class Swatch(val colors: MutableList<ColorItem>, val canonical: ColorItem)
