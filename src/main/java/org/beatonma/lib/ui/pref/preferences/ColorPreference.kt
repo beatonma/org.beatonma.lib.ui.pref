0e6a2647e97751790c7f11fd2cd768ffd6524bf2
Michael Beaton <beatonma@gmail.com>
2018-06-04 13:55:18 +0100

    Implemented preferences for colors, individually or in groups

---
+++ src/main/java/org/beatonma/lib/ui/pref/preferences/ColorPreference.kt
@@ -0,0 +1,82 @@
+package org.beatonma.lib.ui.pref.preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.annotations.SerializedName
+import org.beatonma.lib.log.Log
+import org.json.JSONException
+import org.json.JSONObject
+
+class ColorPreference @Throws(JSONException::class)
+constructor(context: Context,
+            obj: JSONObject) : SimplePreference(context, obj) {
+
+    companion object {
+        private const val TAG = "ColorPreference"
+
+        const val TYPE = "color"
+        const val COLOR = "color"
+        const val SWATCH = "swatch"
+        const val SWATCH_POSITION = "swatch_position"
+    }
+
+    @SerializedName(COLOR)
+    var color: Int = 0
+
+    @SerializedName(SWATCH)
+    var swatch: Int = -1
+
+    @SerializedName(SWATCH_POSITION)
+    var swatchPosition: Int = -1
+
+    init {
+        color = getInt(context, obj.optString(COLOR, "0"))
+        Log.d(TAG, "ColorPreference built with color=$color")
+    }
+
+    override val type: String
+        get() = TYPE
+
+    override fun load(preferences: SharedPreferences) {
+        color = preferences.getInt(key, color)
+        swatch = preferences.getInt("${key}_$SWATCH", -1)
+        swatchPosition = preferences.getInt("${key}_$SWATCH_POSITION", -1)
+    }
+
+    override fun save(editor: SharedPreferences.Editor) {
+        editor.putInt(key, color)
+        editor.putInt("${key}_$SWATCH", swatch)
+        editor.putInt("${key}_$SWATCH_POSITION", swatchPosition)
+    }
+
+    override fun update(value: Int): Boolean {
+        this.color = value
+        return true
+    }
+
+    override fun update(obj: Any?): Boolean {
+        return try {
+            clone(obj as ColorPreference)
+            true
+        } catch (e: Exception) {
+            Log.e(TAG, "cannot clone object $obj")
+            false
+        }
+    }
+
+    fun clone(other: ColorPreference) {
+        this.color = other.color
+        this.swatch = other.swatch
+        this.swatchPosition = other.swatchPosition
+    }
+
+    fun update(color: Int, swatch: Int = -1, swatchPosition: Int = -1) {
+        this.color = color
+        this.swatch = swatch
+        this.swatchPosition = swatchPosition
+    }
+
+    override fun toString(): String {
+        return "ColorPreference(color=$color, swatch=$swatch. swatchPosition=$swatchPosition)"
+    }
+}
---
+++ src/main/java/org/beatonma/lib/ui/pref/preferences/ColorPreferenceGroup.kt
@@ -0,0 +1,109 @@
+package org.beatonma.lib.ui.pref.preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.annotations.SerializedName
+import org.json.JSONException
+import org.json.JSONObject
+
+class ColorPreferenceGroup @Throws(JSONException::class)
+constructor(context: Context,
+            obj: JSONObject
+) : SimplePreference(context, obj), PreferenceContainer {
+
+    companion object {
+        private const val TAG = "ColorPreference"
+
+        const val TYPE = "color_group"
+        const val CHILD_COLORS = "colors"
+    }
+
+    @SerializedName(CHILD_COLORS)
+    val colors: MutableList<ColorPreference> = mutableListOf()
+    val keyMap = mutableMapOf<String, Int>()
+
+    init {
+        obj.optJSONArray(CHILD_COLORS).forEachObj {
+            colors.add(ColorPreference(context, it))
+            for (i in colors.indices) {
+                keyMap[colors[i].key] = i
+            }
+        }
+    }
+
+    override var prefs: String? = null
+        set(value) {
+            super.prefs = value
+            for (c in colors) {
+                c.prefs = value
+            }
+        }
+
+    override val type: String
+        get() = TYPE
+
+    override fun load(preferences: SharedPreferences) {
+        for (p in colors) {
+            p.load(preferences)
+        }
+    }
+
+    override fun save(editor: SharedPreferences.Editor) {
+        for (p in colors) {
+            p.save(editor)
+        }
+    }
+
+    override fun load(context: Context) {
+        val preferences = context.getSharedPreferences(
+                name, Context.MODE_PRIVATE)
+        load(preferences)
+    }
+
+    override fun save(context: Context) {
+        val editor = context.getSharedPreferences(
+                name, Context.MODE_PRIVATE).edit()
+        save(editor)
+        editor.apply()
+    }
+
+    override fun notifyUpdate(key: String, value: Int): Int {
+        if (key in keyMap) {
+            val position = keyMap[key]!!
+            if (colors[position].update(value)) {
+                return position
+            }
+        }
+        return -1
+    }
+
+    override fun notifyUpdate(key: String, value: String): Int {
+        if (key in keyMap) {
+            val position = keyMap[key]!!
+            if (colors[position].update(value)) {
+                return position
+            }
+        }
+        return -1
+    }
+
+    override fun notifyUpdate(key: String, value: Boolean): Int {
+        if (key in keyMap) {
+            val position = keyMap[key]!!
+            if (colors[position].update(value)) {
+                return position
+            }
+        }
+        return -1
+    }
+
+    override fun notifyUpdate(key: String, obj: Any): Int {
+        if (key in keyMap) {
+            val position = keyMap[key]!!
+            if (colors[position].update(obj)) {
+                return position
+            }
+        }
+        return -1
+    }
+}
