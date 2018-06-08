ebc876140f761f5c2b3526fddb3c785ca192c9a9
Michael Beaton <beatonma@gmail.com>
2018-06-08 18:43:58 +0100

    Represents a dependency relationship between preferences

---
+++ src/main/java/org/beatonma/lib/ui/pref/preferences/Dependency.kt
@@ -0,0 +1,18 @@
+package org.beatonma.lib.ui.pref.preferences
+
+import java.io.Serializable
+import java.util.regex.Pattern
+
+data class Dependency(val key: String, val operator: String, val value: String,
+                      var passed: Boolean = true) : Serializable
+
+internal fun parseDependency(dependencyDefinition: String?): Dependency? {
+    if (dependencyDefinition == null) return null
+
+    // (key)(operator)(value)
+    val pattern = Pattern.compile("([\\w\\d_]+)\\s*([!=<>]+)\\s*([\\w\\d-]+)")
+    val m = pattern.matcher(dependencyDefinition)
+    return if (m.matches()) {
+        Dependency(m.group(1), m.group(2), m.group(3))
+    } else null
+}
