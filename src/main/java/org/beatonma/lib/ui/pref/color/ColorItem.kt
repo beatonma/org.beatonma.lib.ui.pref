4fbc2859532bb8776482db99a6faf6b71fff3e1b
Michael Beaton <beatonma@gmail.com>
2018-06-27 11:51:52 +0100

    Removed unnecessary GSON and log module dependencies

--- src/main/java/org/beatonma/lib/ui/pref/color/ColorItem.kt
+++ src/main/java/org/beatonma/lib/ui/pref/color/ColorItem.kt
@@ -1,12 +1,11 @@
 package org.beatonma.lib.ui.pref.color
 
-import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class ColorItem(
-        @SerializedName("color") val color: Int = -1,
-        @SerializedName("selected") var selected: Boolean = false,
-        @SerializedName("selectable") var selectable: Boolean = true    // Placeholder if false
+        val color: Int = -1,
+        var selected: Boolean = false,
+        var selectable: Boolean = true    // Placeholder if false
 ) : Serializable {
     companion object {
         private const val TAG = "ColorItem"
