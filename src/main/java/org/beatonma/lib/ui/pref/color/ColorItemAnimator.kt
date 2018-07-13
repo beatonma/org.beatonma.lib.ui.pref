8450b9730582b4604a3b888c3e882d62f586e3c8
Michael Beaton <beatonma@gmail.com>
2018-07-13 21:42:07 +0100

    - colorAnim() now sets color field on ColorPatchView instead of calling   setBackgroundColor (due to change in ColorPatchView drawing required   by rendering of transparency grid background) - Minor refactoring

--- src/main/java/org/beatonma/lib/ui/pref/color/ColorItemAnimator.kt
+++ src/main/java/org/beatonma/lib/ui/pref/color/ColorItemAnimator.kt
@@ -4,14 +4,14 @@ import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
-import android.annotation.TargetApi
-import android.os.Build
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import org.beatonma.lib.prefs.R
 import org.beatonma.lib.ui.recyclerview.BaseViewHolder
 import org.beatonma.lib.ui.recyclerview.itemanimator.BaseItemAnimator
 import org.beatonma.lib.util.Sdk
+import org.beatonma.lib.util.kotlin.extensions.heightF
+import org.beatonma.lib.util.kotlin.extensions.widthF
 
 /**
  * Implementation assumes the associated RecyclerView uses a GridLayoutManager with an adapter
@@ -21,7 +21,6 @@ import org.beatonma.lib.util.Sdk
  * animateChange -> color transition
  * animateRemove -> shrink out
  */
-
 
 private fun Double.squared(): Double {
     return this * this
@@ -36,10 +35,6 @@ class ColorItemAnimator(
 
     private val epicenter = GridItem(0) // List position from which animations should emanate
     private val tempGridItem = GridItem(0)
-
-    companion object {
-        private const val TAG = "ColorItemAnimator"
-    }
 
     fun setEpicenter(index: Int) {
         epicenter.updateIndex(index)
@@ -98,8 +93,8 @@ class ColorItemAnimator(
         val scale = scaleAnim(patch, 0F, 1F)
         val translate = translateAnim(
                 patch,
-                floatArrayOf(patch.widthF() * ripple * tempGridItem.xDistance, 0F),
-                floatArrayOf(patch.heightF() * ripple * tempGridItem.yDistance, 0F)
+                floatArrayOf(patch.widthF * ripple * tempGridItem.xDistance, 0F),
+                floatArrayOf(patch.heightF * ripple * tempGridItem.yDistance, 0F)
         )
 
         return AnimatorSet().apply {
@@ -108,7 +103,7 @@ class ColorItemAnimator(
             duration = addDuration
             interpolator = enterInterpolator
             startDelay = getDelayFor(tempGridItem.distance)
-            addListener(object: AddAnimatorListenerAdapter(holder) {
+            addListener(object : AddAnimatorListenerAdapter(holder) {
                 override fun onAnimationCancel(animation: Animator?) {
                     super.onAnimationCancel(animation)
                     patch.scaleX = 1F
@@ -133,8 +128,8 @@ class ColorItemAnimator(
         val scale = scaleAnim(patch, 1F, 0F, 0F, 0F)
         val translate = translateAnim(
                 patch,
-                floatArrayOf(0F, patch.widthF() * ripple * tempGridItem.xDistance),
-                floatArrayOf(0F, patch.heightF() * ripple * tempGridItem.yDistance))
+                floatArrayOf(0F, patch.widthF * ripple * tempGridItem.xDistance),
+                floatArrayOf(0F, patch.heightF * ripple * tempGridItem.yDistance))
 
         return AnimatorSet().apply {
             playTogether(translate, scale)
@@ -142,7 +137,7 @@ class ColorItemAnimator(
             duration = addDuration
             interpolator = exitInterpolator
             startDelay = getDelayFor(tempGridItem.distance)
-            addListener(object: RemoveAnimatorListenerAdapter(holder) {
+            addListener(object : RemoveAnimatorListenerAdapter(holder) {
                 override fun onAnimationCancel(animation: Animator?) {
                     super.onAnimationCancel(animation)
                     patch.scaleX = 0F
@@ -180,10 +175,9 @@ class ColorItemAnimator(
 
         val translate = translateAnim(
                 oldView,
-                floatArrayOf(0F, oldView.widthF() * ripple * tempGridItem.xDistance, 0F),
-                floatArrayOf(0F, oldView.heightF() * ripple * tempGridItem.yDistance, 0F)
+                floatArrayOf(0F, oldView.widthF * ripple * tempGridItem.xDistance, 0F),
+                floatArrayOf(0F, oldView.heightF * ripple * tempGridItem.yDistance, 0F)
         )
-
 
         val color = colorAnim(oldView, oldView.color, newView.color)
         return AnimatorSet().apply {
@@ -195,40 +189,62 @@ class ColorItemAnimator(
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun colorAnim(view: View, vararg colors: Int): Animator? {
-        if (!Sdk.isLollipop) return null
-
-        val animator = ValueAnimator.ofArgb(*colors)
-        animator.addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
-            override fun onAnimationUpdate(anim: ValueAnimator?) {
-                view.setBackgroundColor(anim?.animatedValue as Int)
-                if (anim.animatedFraction >= 1F) {
-                    animator.removeUpdateListener(this)
+    private fun colorAnim(view: View, vararg colors: Int): Animator {
+        return (if (Sdk.isLollipop) ValueAnimator.ofArgb(*colors) else ValueAnimator.ofInt(*colors)).apply {
+            addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
+                override fun onAnimationUpdate(anim: ValueAnimator?) {
+                    anim ?: return
+                    (view as? ColorPatchView)?.let { it.color = anim.animatedValue as Int }
+                    if (anim.animatedFraction >= 1F) {
+                        anim.removeUpdateListener(this)
+                    }
                 }
-            }
-        })
-        return animator
+            })
+        }
     }
+//
+//        if (Sdk.isLollipop) {
+//            return ValueAnimator.ofArgb(*colors).apply {
+//                addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
+//                    override fun onAnimationUpdate(anim: ValueAnimator?) {
+//                        anim ?: return
+//                        (view as? ColorPatchView)?.let { it.color = anim.animatedValue as Int }
+//                        if (anim.animatedFraction >= 1F) {
+//                            anim.removeUpdateListener(this)
+//                        }
+//                    }
+//                })
+//            }
+//        } else {
+//            return ValueAnimator.ofInt(*colors).apply {
+//                addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
+//                    override fun onAnimationUpdate(anim: ValueAnimator?) {
+//                        view.setBackgroundColor(anim?.animatedValue as Int)
+//                        if (anim.animatedFraction >= 1F) {
+//                            anim.removeUpdateListener(this)
+//                        }
+//                    }
+//                })
+//            }
+//        }
+//    }
 
     private fun scaleAnim(view: View, vararg values: Float): Animator {
-        val scale = AnimatorSet()
-
-        scale.playTogether(
-                ObjectAnimator.ofFloat(view, "scaleX", *values),
-                ObjectAnimator.ofFloat(view, "scaleY", *values)
-        )
-        return scale
+        return AnimatorSet().apply {
+            playTogether(
+                    ObjectAnimator.ofFloat(view, "scaleX", *values),
+                    ObjectAnimator.ofFloat(view, "scaleY", *values)
+            )
+        }
     }
 
     private fun translateAnim(view: View, xValues: FloatArray, yValues: FloatArray): Animator {
-        val translate = AnimatorSet()
-
-        translate.playTogether(
-                ObjectAnimator.ofFloat(view, "translationX", *xValues),
-                ObjectAnimator.ofFloat(view, "translationY", *yValues)
-        )
-        return translate
+        return AnimatorSet().apply {
+            playTogether(
+                    ObjectAnimator.ofFloat(view, "translationX", *xValues),
+                    ObjectAnimator.ofFloat(view, "translationY", *yValues)
+            )
+        }
     }
 
     inner class GridItem(index: Int) {
@@ -260,7 +276,6 @@ class ColorItemAnimator(
         }
     }
 }
-
 
 abstract class BasePatchViewHolder(
         view: View,
