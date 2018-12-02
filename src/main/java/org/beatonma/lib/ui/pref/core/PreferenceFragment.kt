5ee672b8194c7f41fc89677555f09d4c39729abe
Michael Beaton <beatonma@gmail.com>
2018-12-02 15:57:26 +0000

    Added BasePreferenceFragment so we can create subclasses that load preferences in different ways (primarily for testing)

--- src/main/java/org/beatonma/lib/ui/pref/core/PreferenceFragment.kt
+++ src/main/java/org/beatonma/lib/ui/pref/core/PreferenceFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.Loader
 import org.beatonma.lib.load.Result
+import org.beatonma.lib.load.onComplete
 import org.beatonma.lib.ui.activity.BaseFragment
 import org.beatonma.lib.ui.pref.color.SwatchColorPreferenceActivity
 import org.beatonma.lib.ui.pref.list.AppListPreferenceActivity
@@ -19,13 +20,10 @@ import org.beatonma.lib.ui.pref.preferences.PreferenceGroup
 import java.lang.ref.WeakReference
 
 private const val LOADER_PREFS = 34659
-abstract class PreferenceFragment : BaseFragment(),
-        LoaderManager.LoaderCallbacks<Result<PreferenceGroup>> {
+abstract class BasePreferenceFragment : BaseFragment() {
 
     lateinit var adapter: PreferenceAdapter
     var weakContext: WeakReference<Context>? = null
-
-    abstract val preferenceDefinitions: Int
 
     open fun buildAdapter(): PreferenceAdapter {
         return PreferenceAdapter(this)
@@ -40,7 +38,6 @@ abstract class PreferenceFragment : BaseFragment(),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         adapter.onRestoreInstanceState(savedInstanceState)
-        LoaderManager.getInstance(this).initLoader(LOADER_PREFS, null, this)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -76,28 +73,41 @@ abstract class PreferenceFragment : BaseFragment(),
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        adapter.onSaveInstanceState(outState)
+        super.onSaveInstanceState(outState)
+    }
+}
+
+
+/**
+ * PreferenceFragment implementation that loads preference definitions from a JSON file in
+ * the raw resources directory.
+ */
+abstract class PreferenceFragment : BasePreferenceFragment(),
+        LoaderManager.LoaderCallbacks<Result<PreferenceGroup>> {
+
+    abstract val preferenceDefinitions: Int
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        LoaderManager.getInstance(this).initLoader(LOADER_PREFS, null, this)
+    }
+
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Result<PreferenceGroup>> {
         return context?.let { PreferenceLoader(it, preferenceDefinitions) }
                 ?: throw IllegalStateException() // Context should never be null when this is called
     }
 
     override fun onLoadFinished(loader: Loader<Result<PreferenceGroup>>, result: Result<PreferenceGroup>) {
-        weakContext?.get()?.let {
-            if (result.isComplete) {
-                val data = result.data
-                if (data != null) {
-                    adapter.setPreferences(it, data)
-                }
+        weakContext?.get()?.let {ctx ->
+            result.apply {
+                onComplete { adapter.setPreferences(ctx, it) }
             }
         }
     }
 
     override fun onLoaderReset(loader: Loader<Result<PreferenceGroup>>) {
 
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        adapter.onSaveInstanceState(outState)
-        super.onSaveInstanceState(outState)
     }
 }
