a3ca44d6ace40ab2d90568354f23cd5d6e2f9ff8
Michael Beaton <beatonma@gmail.com>
2018-12-09 16:00:00 +0000

    Base class for preferences that can be dismissed by the user

---
+++ src/main/java/org/beatonma/lib/ui/pref/preferences/DismissiblePreference.kt
@@ -0,0 +1,62 @@
+package org.beatonma.lib.ui.pref.preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.annotation.VisibleForTesting
+import androidx.core.content.edit
+import org.json.JSONObject
+
+internal const val DISMISSIBLE = "dismissible"
+internal const val DISMISSED = "dismissed"
+
+abstract class DismissiblePreference : BasePreference {
+    var dismissible: Boolean = true
+    var dismissed: Boolean = false
+
+    override var allowDisplay: Boolean = true
+        get() = (dependency?.passed ?: true) && !dismissed
+
+    private val BasePreference.dismissedKey: String
+        get() = "${key}_$DISMISSED"
+
+    @VisibleForTesting
+    constructor() : super()
+
+    constructor(source: DismissiblePreference) : super(source) {
+        dismissible = source.dismissible
+        dismissed = source.dismissed
+    }
+
+    constructor(context: Context, obj: JSONObject) : super(context, obj) {
+        dismissible = getBoolean(context, obj.optString(DISMISSIBLE))
+        dismissed = getBoolean(context, obj.optString(DISMISSED))
+    }
+
+    constructor(bundle: Bundle?) : super(bundle) {
+        bundle?.run {
+            dismissible = bundle.getBoolean(DISMISSIBLE)
+            dismissed = bundle.getBoolean(DISMISSED)
+        }
+    }
+
+    override fun load(sharedPreferences: SharedPreferences) {
+        dismissed = sharedPreferences.getBoolean(dismissedKey, false)
+    }
+
+    override fun save(editor: SharedPreferences.Editor) {
+        editor.putBoolean(dismissedKey, dismissed)
+    }
+
+    override fun toBundle(bundle: Bundle) = super.toBundle(bundle).apply {
+        putBoolean(DISMISSIBLE, dismissible)
+        putBoolean(DISMISSED, dismissed)
+    }
+
+    fun dismiss(sharedPreferences: SharedPreferences?) {
+        dismissed = true
+        sharedPreferences?.edit {
+            putBoolean(dismissedKey, true)
+        }
+    }
+}
